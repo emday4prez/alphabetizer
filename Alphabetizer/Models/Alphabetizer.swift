4e472265462f8c9cc5057a99fa4ecfc55a2cfb12
Emerson <emerson.day@gmail.com>
2025-02-16 18:35:28 -0800

    create data model

---
+++ Alphabetizer/Models/Alphabetizer.swift
@@ -0,0 +1,33 @@
+//
+//  Alphabetizer.swift
+//  Alphabetizer
+//
+//
+
+import Foundation
+
+class Alphabetizer {
+    private let tileCount = 3
+    private var vocab: Vocabulary
+
+    var tiles = [Tile]()
+    var score = 0
+    
+    init(vocab: Vocabulary = .oceanAnimals) {
+        self.vocab = vocab
+        startNewGame()
+    }
+    
+    ///check if tiles are in alphabetical order
+    func submit(){
+        // todo: implement submit
+        score += 1
+    }
+    
+    //mark: private implementation
+    
+    /// updates 'titles' with a new set of unalphabetized words
+    private func startNewGame(){
+        
+    }
+}

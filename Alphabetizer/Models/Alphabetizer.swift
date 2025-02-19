70434fef26313b18feebcb9e76f94fd365e781cc
Emerson <emerson.day@gmail.com>
2025-02-18 18:40:34 -0800

    game logic

--- Alphabetizer/Models/Alphabetizer.swift
+++ Alphabetizer/Models/Alphabetizer.swift
@@ -6,30 +6,85 @@
 
 import Foundation
 
+
 @Observable
 class Alphabetizer {
     private let tileCount = 3
     private var vocab: Vocabulary
 
+
     var tiles = [Tile]()
     var score = 0
     var message: Message = .instructions
-    
+
+
     init(vocab: Vocabulary = .oceanAnimals) {
         self.vocab = vocab
         startNewGame()
     }
-    
-    ///check if tiles are in alphabetical order
-    func submit(){
-        // todo: implement submit
-        score += 1
+
+
+    // Alternates true and false
+    private var isAlphabetized = false
+
+
+    /// Checks if tiles are in alphabetical order
+    func submit() {
+        // Check if the tiles are alphabetized
+        // TODO: Compare alphabetical order to position
+        isAlphabetized.toggle()
+
+
+        // If alphabetized, increment the score
+        if isAlphabetized {
+            score += 1
+        }
+
+
+        // Update the message to win or lose
+        message = isAlphabetized ? .youWin : .tryAgain
+
+
+        // Flip over correct tiles
+        for tile in tiles {
+            // TODO: Check if this tile is in the correct position
+            let tileIsAlphabetized = isAlphabetized
+            tile.flipped = tileIsAlphabetized
+        }
+
+
+        Task { @MainActor in
+            // Delay 2 seconds
+            try await Task.sleep(for: .seconds(2))
+
+
+            // If alphabetized, generate new tiles
+            if isAlphabetized {
+                tiles.removeAll()
+                startNewGame()
+            }
+
+
+            // Flip tiles back to words
+            for tile in tiles {
+                tile.flipped = false
+            }
+
+
+            // Display instructions
+            message = .instructions
+        }
     }
-    
-    //mark: private implementation
-    
-    /// updates 'titles' with a new set of unalphabetized words
-    private func startNewGame(){
-        
+
+
+    // MARK: private implementation
+
+
+    /// Updates `tiles` with a new set of unalphabetized words
+    private func startNewGame() {
+        let newWords = vocab.selectRandomWords(count: tileCount)
+        for word in newWords {
+            tiles.append(Tile(word: word))
+        }
     }
 }
